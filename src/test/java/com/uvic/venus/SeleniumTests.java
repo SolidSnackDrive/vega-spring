c1cea07cfdd03dc2b83a297677bc7f2b1bdb5a4b
Graham Stewart <supergproductions@gmail.com>
2022-06-27 21:27:08 -0700

    Added test for registration, working on approval

--- src/test/java/com/uvic/venus/SeleniumTests.java
+++ src/test/java/com/uvic/venus/SeleniumTests.java
@@ -148,4 +148,38 @@ public class SeleniumTests{
         Select roleUSER = new Select(roleSelect);
         roleUSER.selectByValue("ROLE_USER");
     }
+
+    @Test
+    public void testRegistration() {
+        WebElement signup = driver.findElement(By.xpath("/html/body/div/div/div[1]/div[1]/nav[2]/div/div/div[2]/a"));
+        assertEquals("Signup", signup.getText(), "Button not fonud");
+        signup.click();
+
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/form/div[1]/input")).sendKeys("selenium@test.com");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/form/div[2]/input")).sendKeys("Sel");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/form/div[3]/input")).sendKeys("Enium");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/form/div[4]/input")).sendKeys("pass" + Keys.ENTER);
+
+        WebElement success = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/h6"));
+        assertEquals("User created successfully!", success.getText());
+    }
+
+    @Test
+    public void testApproval() {
+        // Find login button
+        WebElement login = driver.findElement(By.xpath("/html/body/div/div/div[1]/div[1]/nav[2]/div/div/div/a"));
+        assertEquals("Login/SignUp", login.getText(), "Button not found");
+
+        login.click();
+
+        // Login using ADMIN credentials
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/form/div[1]/input")).sendKeys("admin@venus.com");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/form/div[2]/input")).sendKeys("pass" + Keys.ENTER);
+
+        // Check if buttons exits
+        WebElement admin = driver.findElement(By.xpath("/html/body/div/div/div[1]/div[1]/nav[2]/div/div/a[7]"));
+        assertEquals("Admin", admin.getText(), "Button not found");
+
+        admin.click();
+    }
 }
