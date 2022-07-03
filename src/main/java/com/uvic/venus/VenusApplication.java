d5c6be1353d55362fad5dc2d0f296bb499f12853
Graham Stewart <supergproductions@gmail.com>
2022-07-02 18:56:25 -0700

    Added additional tests related to file uploading and file listing

--- src/main/java/com/uvic/venus/VenusApplication.java
+++ src/main/java/com/uvic/venus/VenusApplication.java
@@ -1,9 +1,12 @@
 package com.uvic.venus;
 
 import com.uvic.venus.storage.StorageProperties;
+import com.uvic.venus.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -11,10 +14,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EnableConfigurationProperties(StorageProperties.class)
 @PropertySource("classpath:application-dev.properties")
+
 public class VenusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VenusApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
