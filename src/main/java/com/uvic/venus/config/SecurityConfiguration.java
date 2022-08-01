f0b7a36599bcd4f5707ff6d8dfb9e5a684062323
mschaldias <mschaldias@uvic.ca>
2022-08-01 11:57:28 -0700

    added BCrypt password encoder

--- src/main/java/com/uvic/venus/config/SecurityConfiguration.java
+++ src/main/java/com/uvic/venus/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
-                .dataSource(dataSource);
+                .dataSource(dataSource).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
